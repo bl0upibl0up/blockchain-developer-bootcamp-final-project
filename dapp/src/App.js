f73fa6709278f52cd24eec791a2aeedf8f03be42
bl0upibl0up <moreau.ambroise@gmail.com>
2021-11-25 21:28:35 +0100

    cleaning

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Web3ReactProvider } from '@web3-react/core';
 import { ethers } from 'ethers';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import { AppContextProvider } from './AppContext';
