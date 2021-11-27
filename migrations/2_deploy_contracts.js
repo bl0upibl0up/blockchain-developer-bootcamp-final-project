c37ea628849df86ad3ebf3f204f8ce58fefe3b0a
bl0upibl0up <moreau.ambroise@gmail.com>
2021-11-27 02:23:09 +0100

    remove abi

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 const FestivalMarketToken = artifacts.require('FestivalMarketToken');
 const FestivalMarketPlace = artifacts.require("FestivalMarketPlace");
-
+const fs = require('fs');
 module.exports = async function(deployer, networks, accounts){
     await deployer.deploy(FestivalMarketToken);
     await deployer.deploy(FestivalMarketPlace);
